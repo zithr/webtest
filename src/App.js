8e9c1a0548964c7977ea14f17183623ad8869c3e
BurnySc2 <gamingburny@gmail.com>
2021-02-18 19:03:30 +0100

    Apply prettier, remove main.css

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import ImageCard from './components/ImageCard';
-import ImageSearch from './components/ImageSearch'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import About from './components/About'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-
+import React, { useState, useEffect } from "react"
+import ImageCard from "./components/ImageCard"
+import ImageSearch from "./components/ImageSearch"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState('');
+    const [images, setImages] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [term, setTerm] = useState("")
 
-  useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
-      .then(res => res.json())
-      .then(data => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch(err => console.log(err));
-  }, [term]);
+    useEffect(() => {
+        fetch(
+            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setImages(data.hits)
+                setIsLoading(false)
+            })
+            .catch((err) => console.log(err))
+    }, [term])
 
-  return (
-    <Router>
-    <div>
-    <Header />
-    <div className="container mx-auto">     
-    
-    <Route path='/' exact render={(props) => (
-      <>
-        <ImageSearch searchText={(text) => setTerm(text)} />
+    return (
+        <Router>
+            <div>
+                <Header />
+                <div className="container mx-auto">
+                    <Route
+                        path="/"
+                        exact
+                        render={(props) => (
+                            <>
+                                <ImageSearch searchText={(text) => setTerm(text)} />
 
-        {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32 bg-gray-60">No Images Found</h1>}
-        {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32 bg-gray-60">Images loading...</h1> : <div className="grid grid-cols-5 gap-3">
-          {images.map(image => (
-            <ImageCard key={image.id} image={image} />
-  ))}
-</div>}
-      </>
-    )} />
-    </div>
-    <Route path='/about' component={About} />
-    <Footer />
-    </div>
-    </Router>
-  );
+                                {!isLoading && images.length === 0 && (
+                                    <h1 className="text-5xl text-center mx-auto mt-32 bg-gray-60">
+                                        No Images Found
+                                    </h1>
+                                )}
+                                {isLoading ? (
+                                    <h1 className="text-6xl text-center mx-auto mt-32 bg-gray-60">
+                                        Images loading...
+                                    </h1>
+                                ) : (
+                                    <div className="grid grid-cols-5 gap-3">
+                                        {images.map((image) => (
+                                            <ImageCard key={image.id} image={image} />
+                                        ))}
+                                    </div>
+                                )}
+                            </>
+                        )}
+                    />
+                </div>
+                <Route path="/about" component={About} />
+                <Footer />
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
 const ImageSearch = ({ searchText }) => {
-    const [text, setText] = useState('');
+    const [text, setText] = useState("")
     const onSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        searchText(text);
+        searchText(text)
     }
 
     return (
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
             <form onSubmit={onSubmit} className="w-full max-w-sm">
                 <div className="flex items-center border-b-2 border-purple-200 py-2">
-                <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for images..." />
-                <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
-            Search
-            </button>
-            </div>
+                    <input
+                        onChange={(e) => setText(e.target.value)}
+                        className="appearance-none bg-transparent w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                        type="text"
+                        placeholder="Search for images..."
+                    />
+                    <button
+                        className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded"
+                        type="submit"
+                    >
+                        Search
+                    </button>
+                </div>
             </form>
         </div>
     )
 }
 
-export default ImageSearch;
+export default ImageSearch
