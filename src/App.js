cf182e928f45d30d825b6eca6288176cd78c0412
zithr <zither.sorrow@gmail.com>
2021-02-19 06:56:31 +0000

    add infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react"
+import { BottomScrollListener } from "react-bottom-scroll-listener"
 import ImageCard from "./components/ImageCard"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -9,59 +10,74 @@ function App() {
     const [images, setImages] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [term, setTerm] = useState("")
-    const page = 1
-
-    let fetchItems = async () => {
+    const atBottom = () => {
+        setPage(page + 1)
+        console.log("heyguysatbottom")
+    }
+    const [page, setPage] = useState(1)
+    let fetchItems = async (fetchPage) => {
         let response = await fetch(
-            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&page=${page}`
+            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true&page=${fetchPage}`
         )
         if (response.ok) {
             let data = await response.json()
-            setImages(data.hits)
+            setImages([...images, ...data.hits])
             setIsLoading(false)
         } else {
             console.log(response.status)
         }
     }
+    useEffect(() => {
+        setImages([])
+        if (page === 1) {
+            fetchItems(page)
+        } else {
+            setPage(1)
+        }
+        setImages([])
+    }, [term])
 
     useEffect(() => {
-        fetchItems()
-    }, [term])
+        fetchItems(page)
+    }, [page])
 
     return (
         <div>
             <Router>
-                <Header setTerm={setTerm}/>
+                <Header setTerm={setTerm} />
+
                 <Route
                     path="/"
                     exact
                     render={(props) => (
-                        <div className="container mx-auto">
-                            <div>
-
-                                {!isLoading && images.length === 0 && (
-                                    <h1 className="text-5xl text-center mx-auto mt-32 bg-gray-60">
-                                        No Images Found
-                                    </h1>
-                                )}
-                                {isLoading ? (
-                                    <h1 className="text-6xl text-center mx-auto mt-32 bg-gray-60">
-                                        Images loading...
-                                    </h1>
-                                ) : (
-                                    <div className="grid grid-cols-4 gap-3">
-                                        {images.map((image) => (
-                                            <ImageCard key={image.id} image={image} />
-                                        ))}
-                                    </div>
-                                )}
+                        <>
+                            <div className="container mx-auto">
+                                <div>
+                                    {!isLoading && images.length === 0 && (
+                                        <h1 className="text-5xl text-center mx-auto mt-32 bg-gray-60">
+                                            No Images Found
+                                        </h1>
+                                    )}
+                                    {isLoading ? (
+                                        <h1 className="text-6xl text-center mx-auto mt-32 bg-gray-60">
+                                            Images loading...
+                                        </h1>
+                                    ) : (
+                                        <div className="grid grid-cols-4 gap-3">
+                                            {images.map((image) => (
+                                                <ImageCard key={image.id} image={image} />
+                                            ))}
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
+                        </>
                     )}
                 />
                 <Route path="/about" component={About} />
                 <Footer />
             </Router>
+            <BottomScrollListener onBottom={atBottom} />
         </div>
     )
 }
