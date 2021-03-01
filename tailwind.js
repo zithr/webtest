90dc5345aa0f49e31f1f5c31a2ac5cacbb3bb7f6
zithr <zither.sorrow@gmail.com>
2021-03-01 08:48:12 +0000

    add tailwind spacing

--- tailwind.js
+++ tailwind.js
@@ -63,6 +63,7 @@ module.exports = {
             72: "18rem",
             80: "20rem",
             96: "24rem",
+            112: "28rem",
         },
         animation: {
             none: "none",
