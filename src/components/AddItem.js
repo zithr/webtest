cf5ddd51bb931672791023385d29713ef74b3d18
zithr <zither.sorrow@gmail.com>
2021-03-10 17:54:30 +0000

    move svg locations

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,16 +4,16 @@ import ReactTooltip from "react-tooltip"
 import { images } from "./images/images.js"
 
 const AddItem = () => {
-    const [previewFile, setPreviewFile] = useState([])
+    const [uploadFiles, setUploadFiles] = useState([])
 
     const changeHandler = (event) => {
-        setPreviewFile([...previewFile, URL.createObjectURL(event.target.files[0])])
-        console.log(previewFile)
+        setUploadFiles([...uploadFiles, URL.createObjectURL(event.target.files[0])])
     }
 
     const handleSubmission = () => {}
     return (
         <div className="flex flex-1 justify-center bg-yellow-50">
+            <ReactTooltip />
             <div className="font-serif">
                 <div className="mt-2 bg-white rounded shadow-lg p-8">
                     <div className="flex">
@@ -29,37 +29,35 @@ const AddItem = () => {
                     </div>
                     <div className="">
                         <div className="mt-3 flex">
-                            {previewFile.map((item, index) => (
-                                <div
-                                    className="flex pr-1 w-24 h-24 justify-center cursor-pointer"
-                                    // Clicking on the image removes it from 'previewFile' array
-                                    onClick={() => {
-                                        setPreviewFile([
-                                            ...previewFile.slice(0, index),
-                                            ...previewFile.slice(index + 1),
-                                        ])
-                                    }}
-                                >
-                                    <img
-                                        src={item}
-                                        alt=""
-                                        className="flex object-cover w-full justify-center"
-                                    />
+                            {uploadFiles.map((item, index) => (
+                                <div className="flex pr-1 justify-center">
+                                    <div className="w-24 h-24 flex-col justify-items-center">
+                                        <img
+                                            src={item}
+                                            alt=""
+                                            className="flex object-cover w-full justify-center"
+                                        />
+                                        <div
+                                            className="flex justify-center cursor-pointer"
+                                            // Clicking removes it from 'uploadFiles' array
+                                            onClick={() => {
+                                                setUploadFiles([
+                                                    ...uploadFiles.slice(0, index),
+                                                    ...uploadFiles.slice(index + 1),
+                                                ])
+                                            }}
+                                        >
+                                            {images.xsign}
+                                        </div>
+                                    </div>
                                 </div>
                             ))}
                         </div>
-                        <ReactTooltip />
-                        <div class="mt-2 flex items-center justify-center ">
+
+                        <div class="flex items-center justify-center ">
                             <label class="border-2 border-purple-100 w-36 flex flex-col items-center p-2 bg-white text-purple-300 rounded-lg shadow-md cursor-pointer hover:bg-blue hover:text-purple-500 hover:border-purple-300">
-                                <svg
-                                    class="w-6 h-6"
-                                    fill="currentColor"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                </svg>
-                                <span class="mt-2 text-base leading-normal">Upload Images</span>
+                                {images.uploadicon}
+                                <span class="text-base leading-normal">Upload Images</span>
                                 <input type="file" class="hidden" onChange={changeHandler} />
                             </label>
                         </div>
@@ -72,30 +70,9 @@ const AddItem = () => {
                                 type="text"
                                 required="true"
                             />
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 32 32"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="stroke-current text-purple-600 stroke-1"
-                                data-tip="A name to help you and others quickly identify your item"
-                            >
-                                <path
-                                    d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
-                                    stroke-miterlimit="10"
-                                />
-                                <path
-                                    d="M11 16H21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                                <path
-                                    d="M16 11V21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
+                            <span data-tip="A name to help you and others quickly identify your item">
+                                {images.circleplus}
+                            </span>
                             <label>Category</label>
                             <select>
                                 <option value="Books">Books</option>
@@ -106,30 +83,7 @@ const AddItem = () => {
                                 <option value="Jewellery">Jewellery</option>
                                 <option value="Toys">Toys</option>
                             </select>
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 32 32"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="stroke-current text-purple-600 stroke-1"
-                                data-tip="Type of item"
-                            >
-                                <path
-                                    d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
-                                    stroke-miterlimit="10"
-                                />
-                                <path
-                                    d="M11 16H21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                                <path
-                                    d="M16 11V21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
+                            <span data-tip="Type of item">{images.circleplus}</span>
                             <label>Tags</label>
                             <input
                                 className="focus:outline-none border-2 focus:border-purple-500 px-1"
@@ -137,8 +91,9 @@ const AddItem = () => {
                                 required="true"
                                 placeholder="#blue #bird #sky"
                             />
-                            {images.plussign}
-
+                            <span data-tip="Tags to help others find this type of item">
+                                {images.plussign}
+                            </span>
                             <label>Price</label>
                             <input
                                 className="focus:outline-none border-2 focus:border-purple-500 px-1"
@@ -146,30 +101,7 @@ const AddItem = () => {
                                 required="true"
                                 placeholder="0.00"
                             />
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 32 32"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="stroke-current text-purple-600 stroke-1"
-                                data-tip="Price your item"
-                            >
-                                <path
-                                    d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
-                                    stroke-miterlimit="10"
-                                />
-                                <path
-                                    d="M11 16H21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                                <path
-                                    d="M16 11V21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
+                            <span data-tip="Price your item">{images.circleplus}</span>
                             <label>Description</label>
                             <textarea
                                 className="focus:outline-none border-2 focus:border-purple-500 px-1"
@@ -178,30 +110,10 @@ const AddItem = () => {
                                 type="text"
                                 required="true"
                             />
-                            <svg
-                                width="16"
-                                height="16"
-                                viewBox="0 0 32 32"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="stroke-current text-purple-600 stroke-1"
-                                data-tip="Provide any relevant additional information about your item, e.g. defects, stains, tears etc."
-                            >
-                                <path
-                                    d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
-                                    stroke-miterlimit="10"
-                                />
-                                <path
-                                    d="M11 16H21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                                <path
-                                    d="M16 11V21"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                />
-                            </svg>
+                            <span data-tip="Provide any relevant additional information about your item, e.g. defects, stains, tears etc.">
+                                {images.circleplus}
+                            </span>
+
                             <button
                                 className="bg-purple-500 text-2xl col-span-2 rounded-lg shadow-xl text-gray-100 hover:bg-purple-600"
                                 data-tip="List your item in your shop!"
--- src/components/images/images.js
+++ src/components/images/images.js
@@ -9,7 +9,6 @@ export const images = {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className="stroke-current text-purple-600 stroke-1"
-            data-tip="Tags to help others find this type of item"
         >
             <path
                 d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
@@ -17,6 +16,49 @@ export const images = {
             />
             <path d="M11 16H21" stroke-linecap="round" stroke-linejoin="round" />
             <path d="M16 11V21" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    ),
+    xsign: (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current text-red-400 stroke-2"
+            data-tip="Remove image"
+        >
+            <path
+                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                fill="black"
+            />
+        </svg>
+    ),
+    circleplus: (
+        <svg
+            width="16"
+            height="16"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current text-purple-600 stroke-1"
+        >
+            <path
+                d="M16 28C22.6274 28 28 22.6274 28 16C28 9.37258 22.6274 4 16 4C9.37258 4 4 9.37258 4 16C4 22.6274 9.37258 28 16 28Z"
+                stroke-miterlimit="10"
+            />
+            <path d="M11 16H21" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M16 11V21" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    ),
+    uploadicon: (
+        <svg
+            class="w-6 h-6"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+        >
+            <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
         </svg>
     ),
 }
